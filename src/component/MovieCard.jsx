d756f8561cf3aa7331417745d84183b8b033f90c
Salman-Ahmad-Khan <connect.celman@gmail.com>
2024-11-10 16:19:44 +0530

    fixed bugs

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -3,17 +3,14 @@ import React from 'react';
 const MovieCard = ({ movie: { Year, Poster, Title, Type } }) => {
   return (
     <div className="movie">
-      {/* Display movie release year */}
       <div>
-        <p>{Year}</p>
+        <p className="bold-text">{Year}</p>
       </div>
 
-      {/* Display movie poster or placeholder if unavailable */}
       <div>
         <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
       </div>
 
-      {/* Display movie type and title */}
       <div>
         <span>{Type}</span>
         <h3>{Title}</h3>
